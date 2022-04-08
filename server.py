cac06ba48a1345bacf506abdac6282e1411f2e0c
Ashleigh Ligon <ashleigh@axios.com>
2022-04-08 14:21:26 -0400

    grab content from dc things to do and sports sections

--- server.py
+++ server.py
@@ -1,11 +1,39 @@
+import pprint
 import requests
 
 
 def main():
-    resp = requests.get(
+    things_to_do_resp = requests.get(
         "https://api.axios.com/api/render/stream/content?audience_slug=washington-dc&topic_slug=things-to-do"
-    )
-    print(resp.json())
+    ).json()["results"]
+    sports_resp = requests.get(
+        "https://api.axios.com/api/render/stream/content?audience_slug=washington-dc&topic_slug=sports"
+    ).json()["results"]
+
+    things_to_do_content = []
+    for id in things_to_do_resp:
+        resp = requests.get(f"https://api.axios.com/api/render/content/{id}/")
+        things_to_do_content.append(
+            {
+                "headline": resp.json().get("headline"),
+                "permalink": resp.json().get("permalink"),
+                "published": resp.json().get("published_date"),
+            }
+        )
+
+    sports_content = []
+    for id in sports_resp:
+        resp = requests.get(f"https://api.axios.com/api/render/content/{id}/")
+        sports_content.append(
+            {
+                "headline": resp.json().get("headline"),
+                "permalink": resp.json().get("permalink"),
+                "published": resp.json().get("published_date"),
+            }
+        )
+
+    resp = {"Things To Do": things_to_do_content, "Sports": sports_content}
+    pprint.pprint(resp)
 
 
 if __name__ == "__main__":
