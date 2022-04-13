2ebdc2f65479c10072bc50b3d814b7add2219e00
Ashleigh Ligon <ashleigh@axios.com>
2022-04-13 18:04:08 -0400

    connect bus/metro data to html

--- web/app.py
+++ web/app.py
@@ -11,8 +11,15 @@ def index():
     )
     events = server.get_local_event_data("20001", 50)
     weather = server.get_local_weather_data("20001")
+    bus_data = server.get_bus_alert_data()
+    metro_data = server.get_metro_alert_data()
     return render_template(
-        "hack.html", dash_content=dash_content, events=events["events"], weather=weather
+        "hack.html",
+        dash_content=dash_content,
+        events=events["events"],
+        weather=weather,
+        bus_data=bus_data,
+        metro_data=metro_data,
     )
 
 
