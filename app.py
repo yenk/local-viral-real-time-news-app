aa989ab41b070104a4f8961f4a001593ef2d2a04
yenkie <yen.kha@axios.com>
2022-04-13 16:04:32 -0700

    redeploy app

--- app.py
+++ app.py
@@ -1,67 +1,27 @@
-import dash
-import dash_core_components as dcc
-import dash_bootstrap_components as dbc
-import dash_html_components as html
-import numpy as np
-import pandas as pd
-import matplotlib as mpl
-import gunicorn                     #whilst your local machine's webserver doesn't need this, Heroku's linux webserver (i.e. dyno) does
-from whitenoise import WhiteNoise   #for serving static files on Heroku
+from flask import Flask, render_template
+from backend import server
+from dashy import app
 
-# Instantiate dash app
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY] ) 
+app = Flask(__name__)
 
-# Define the underlying flask app (Used by gunicorn webserver in Heroku production deployment)
-server = app.server 
+@app.route("/")
+def index():
+    dash_content = server.get_dash_content(
+        "Washington DC", ["Things to Do", "Food and Drink"]
+    )
+    events = server.get_local_event_data("20001", 50)
+    weather = server.get_local_weather_data("20001")
+    bus_data = server.get_bus_alert_data()
+    metro_data = server.get_metro_alert_data()
+    return render_template(
+        "hack.html",
+        dash_content=dash_content,
+        events=events["events"],
+        weather=weather,
+        bus_data=bus_data,
+        metro_data=metro_data,
+    )
 
-# Enable Whitenoise for serving static files from Heroku (the /static folder is seen as root by Heroku) 
-server.wsgi_app = WhiteNoise(server.wsgi_app, root='static/') 
 
-# Define Dash layout
-def create_dash_layout(app):
-
-    # Set browser tab title
-    app.title = "Welcome to Axios Local DC's News!" #browser tab
-    
-    # Header
-    header = html.Div([html.Br(), dcc.Markdown(""" # Welcome to Axios Best and Foremost DC's Local News! """), html.Br()])
-    
-    # Body 
-    body = html.Div([dcc.Markdown(""" See What's Happening in DC! """), html.Br(), html.Img(src='dc_heading.png')])
-
-    # Footer
-    footer = html.Div([html.Br(), html.Br(), dcc.Markdown(""" ### Built at ![Image](logo.png) with ![Image](heart.png) using [Dash](https://plotly.com/dash/) and [Heroku](https://devcenter.heroku.com/) """)])
-    
-    # Assemble dash layout 
-    app.layout = html.Div([header, body, footer])
-
-    return app
-
-# Construct the dash layout
-create_dash_layout(app)
-
-# Run flask app
 if __name__ == "__main__":
-    app.run_server(debug=False, host='0.0.0.0', port=8050)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    app.run(debug=True)
--- app/main.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
-import server 
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-  return render_template('hack.html', me="lizna")
