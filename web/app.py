f308ab3d9dd51c354b5c034faa4c9841ee1e553b
Ashleigh Ligon <ashleigh@axios.com>
2022-04-13 16:24:13 -0400

    create python modules, connect dash content to html

--- web/app.py
+++ web/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, redirect, url_for, request, flash
-import server 
+from flask import Flask, render_template
+from backend import server
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('hack.html', me="lizna")
 
-if __name__ == '__main__':
-  app.run(debug=True)
+@app.route("/")
+def index():
+    dash_content = server.get_dash_content(
+        "Washington DC", ["Things to Do", "Food and Drink"]
+    )
+    return render_template("hack.html", dash_content=dash_content)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
