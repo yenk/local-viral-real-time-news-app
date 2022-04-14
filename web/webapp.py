308b3e261a6b77f5656d7e04631d2ed86b4c303f
Ashleigh Ligon <ashleigh@axios.com>
2022-04-14 13:54:03 -0400

    add more covid data

--- web/webapp.py
+++ web/webapp.py
@@ -10,6 +10,8 @@ def fetch_page():
     weather = server.get_local_weather_data("20001")
     bus_data = server.get_bus_alert_data()
     metro_data = server.get_metro_alert_data()
+    covid_cases = server.get_current_day_covid_cases("District of Columbia")
+    covid_week_avg = server.get_week_avg_covid_cases("District of Columbia")
     return render_template(
         "hack.html",
         dash_content=dash_content,
@@ -17,4 +19,6 @@ def fetch_page():
         weather=weather,
         bus_data=bus_data,
         metro_data=metro_data,
+        covid_cases=covid_cases,
+        covid_week_avg=covid_week_avg,
     )
